678db179177e6a63ed3b27869e8176674663dae1
Adamugy <radamugysarme45@hotmail.com>
2022-06-02 00:02:13 +0200

    upload1

---
+++ src/main/java/mz/co/isutc/si/i42/connect/Loginserv.java
@@ -0,0 +1,67 @@
+package mz.co.isutc.si.i42.connect;
+
+import jakarta.servlet.http.HttpServlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+
+/**
+ * Servlet implementation class Loginserv
+ */
+@WebServlet("/Loginserv")
+public class Loginserv extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	public void init() {
+	
+	}
+       
+   
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter printWriter = response.getWriter();
+		
+		try {
+			response.setContentType("text/html");
+			
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/bancodado?useSSL=false", "root", "");
+			Statement statement = connection.createStatement();
+			String str1 = request.getParameter("username");
+			String str2 = request.getParameter("password");
+			String str3  = "select * from login where username='" + str1 + "' AND password='" + str2 + "'";
+			
+			ResultSet resultset = statement.executeQuery(str3);
+			
+			if (resultset.next()) {
+				String str4 = resultset.getString("username");
+				HttpSession httpSession = request.getSession();
+				httpSession.setAttribute("name", str4);
+				printWriter.println("Welcome, " + str4.toUpperCase());
+		        printWriter.println("<a href='chat.jsp'>Vamos no Chat Room</a>");
+		        printWriter.println("<a href='exit.jsp' class='logout-chat'>Logout</a>");
+			} else {
+				printWriter.println("Incorrect Username or Password.");
+			}
+			
+			connection.close();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			
+		}
+				
+	}
+	
+
+}
--- src/main/webapp/chat.jsp
+++ src/main/webapp/chat.jsp
@@ -8,10 +8,44 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script type="text/javascript" src="addText.js"></script>
     <link rel="stylesheet" href="style.css" />
     <title>ChatRoom Application</title>
 </head>
+<script>
+        function texte()
+    {
+        var username = document.getElementById('usermsg').value;
+        var msg = document.getElementById('submitmsg').value;
+        olist = document.getElementById('list');
+        op = document.createElement('p');
+        op.innerHTML = username + "- <g>" + msg + "</g>";
+        ocontent = document.getElementById('chatbox');
+        ocontent.appendChild(op);             //Add new line on click
+        olist.scrollTop = olist.scrollHeight; //Adjust Height
+        
+        var xmlhttp;
+        if (window.XMLHttpRequest)
+        {// code for IE7+, Firefox, Chrome, Opera, Safari
+            xmlhttp = new XMLHttpRequest();
+        }
+        else
+        {// code for IE6, IE5
+            xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+        }
+
+        xmlhttp.open("POST", "chatserv?username=" + username + "&msg=" + msg, true);
+
+        xmlhttp.onreadystatechange = function()
+        {
+            if (xmlhttp.readyState === 4 && xmlhttp.status === 200)
+            {
+                document.getElementById("result").innerHTML = 'sent';
+                document.getElementById("result").innerHTML = '';
+            }
+        };
+        xmlhttp.send(null);
+}
+</script>
 <body>
     <div id="wrapper">
         <div id="menu">
